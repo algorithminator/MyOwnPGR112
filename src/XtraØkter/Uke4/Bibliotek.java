f002334b5e0dd38355c321118466a3ac9387e4c3
algoritminator <frederic.dorn@kristiania.no>
2025-01-18 13:44:03 +0100

    Utlaansystem versjon 1

--- src/XtraØkter/Uke4/Bibliotek.java
+++ src/XtraØkter/Uke4/Bibliotek.java
@@ -15,28 +15,40 @@ public class Bibliotek {
         return boekerLedig;
     }
 
-    public void setBoekerLedig(ArrayList<BibliotekBestand> boekerLedig) {
-        this.boekerLedig = boekerLedig;
+    public void returnerBok(BibliotekBestand bibBok) {
+        this.boekerLedig.add(bibBok);
+        this.boekerUtlaan.remove(bibBok);
     }
 
     public ArrayList<BibliotekBestand> getBoekerUtlaan() {
         return boekerUtlaan;
     }
 
-    public void setBoekerUtlaan(ArrayList<BibliotekBestand> boekerUtlaan) {
-        this.boekerUtlaan = boekerUtlaan;
+    public boolean laanBok(BibliotekBestand bibBok) {
+        if (this.boekerLedig.contains(bibBok)) {
+            this.boekerLedig.remove(bibBok);
+            this.boekerUtlaan.add(bibBok);
+        return true;
+        }
+        else {
+            System.out.println("Bok " + bibBok + " er ikke ledig.");
+            return false;
+        }
     }
 
     public static void main(String[] args) {
-        BibliotekBestand bibBok;
+
         Bibliotek bergenFolkeBib = new Bibliotek();
 
-        //System.out.println("Bergen Bibliotek total bestand: ");
-        //for( var bestand : BibliotekBestand.values())
-        //    System.out.print(" "+bestand);
-        System.out.println(" ");
         System.out.println("Bergen Bibliotek ledige boeker:"+ bergenFolkeBib.getBoekerLedig());
         System.out.println("Bergen Bibliotek utlaante boeker: "+bergenFolkeBib.getBoekerUtlaan());
+        bergenFolkeBib.laanBok(BibliotekBestand.MOMO);
+        System.out.println("Bergen Bibliotek utlaante boeker: "+bergenFolkeBib.getBoekerUtlaan());
+        bergenFolkeBib.laanBok(BibliotekBestand.KARDEMOMMEBY);
+        System.out.println("Bergen Bibliotek utlaante boeker: "+bergenFolkeBib.getBoekerUtlaan());
+        bergenFolkeBib.laanBok(BibliotekBestand.MOMO);
+        System.out.println("Bergen Bibliotek utlaante boeker: "+bergenFolkeBib.getBoekerUtlaan());
+
 
 
         //for(var ledig: bergenFolkeBib.getBoekerUtlaan())
