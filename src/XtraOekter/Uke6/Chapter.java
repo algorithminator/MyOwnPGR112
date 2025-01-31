a36b13c45b9625fe91797cb03763b2cb1cffd457
algoritminator <frederic.dorn@kristiania.no>
2025-01-31 16:24:53 +0100

    Oppgave 2 H) ..
    Chapter + init vars + getter/setter..

---
+++ src/XtraOekter/Uke6/Chapter.java
@@ -0,0 +1,31 @@
+package XtraOekter.Uke6;
+
+public class Chapter {
+    private String title;
+    private int numPages;
+    private int timeReading;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public int getNumPages() {
+        return numPages;
+    }
+
+    public void setNumPages(int numPages) {
+        this.numPages = numPages;
+    }
+
+    public int getTimeReading() {
+        return timeReading;
+    }
+
+    public void setTimeReading(int timeReading) {
+        this.timeReading = timeReading;
+    }
+}
