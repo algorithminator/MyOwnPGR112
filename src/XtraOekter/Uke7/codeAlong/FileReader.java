22dc7ed295d2fb70bb4650d83a93a5d16c7150db
frede <frederic.b.dorn@gmail.com>
2025-02-11 19:59:54 +0100

    First slide code along -> scanner uten checked exceptions

---
+++ src/XtraOekter/Uke7/codeAlong/FileReader.java
@@ -0,0 +1,11 @@
+package XtraOekter.Uke7.codeAlong;
+
+import java.io.File;
+import java.util.Scanner;
+
+public class FileReader {
+    public void dummyMethod(){
+        File myFile = new File("eksempel.txt");
+        Scanner myScanner = new Scanner(myFile);
+    }
+}
