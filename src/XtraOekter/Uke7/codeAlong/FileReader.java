a29880f876967e87b9a158c373d08c7258618e18
frede <frederic.b.dorn@gmail.com>
2025-02-11 20:31:52 +0100

    kast runtime exception

--- src/XtraOekter/Uke7/codeAlong/FileReader.java
+++ src/XtraOekter/Uke7/codeAlong/FileReader.java
@@ -5,18 +5,23 @@ import java.io.FileNotFoundException;
 import java.util.Scanner;
 
 public class FileReader {
+
     public void dummyMethod(){
-        File myFile = new File("artister.txt");
+        File myFile = new File("files/artister.txt");
         System.out.println(myFile.getAbsolutePath());
         Scanner myScanner = null;
         try {
             myScanner = new Scanner(myFile);
             while (myScanner.hasNextLine()){
                 System.out.println(myScanner.nextLine());
+                var x = 4 /0;
             }
+
         } catch (FileNotFoundException e) {
             //System.out.println(e);
             System.out.println("Finner ikke filen. Vennligst sjekk stien + fil");
+        } catch (RuntimeException e) {
+            throw new RuntimeException(e);
         }
         finally {
             if (myScanner != null) myScanner.close();
