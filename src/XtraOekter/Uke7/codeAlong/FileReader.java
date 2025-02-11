380f71a9fdc1ba829ad72711d8c0e90f5ef86517
frede <frederic.b.dorn@gmail.com>
2025-02-11 20:23:32 +0100

    First slide code along ... definer egen filenotfound

--- src/XtraOekter/Uke7/codeAlong/FileReader.java
+++ src/XtraOekter/Uke7/codeAlong/FileReader.java
@@ -6,7 +6,7 @@ import java.util.Scanner;
 
 public class FileReader {
     public void dummyMethod(){
-        File myFile = new File("files/artister.txt");
+        File myFile = new File("artister.txt");
         System.out.println(myFile.getAbsolutePath());
         try {
             Scanner myScanner = new Scanner(myFile);
@@ -14,6 +14,8 @@ public class FileReader {
                 System.out.println(myScanner.nextLine());
             }
         } catch (FileNotFoundException e) {
+            //System.out.println(e);
+            System.out.println("Finner ikke filen. Vennligst sjekk stien + fil");
         }
     }
 }
