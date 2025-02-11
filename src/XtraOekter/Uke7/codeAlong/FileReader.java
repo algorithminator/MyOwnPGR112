b60f43110e331305fc77f51bcc663069d6a75ffb
frede <frederic.b.dorn@gmail.com>
2025-02-11 20:06:11 +0100

    First slide code along ... txt fil

--- src/XtraOekter/Uke7/codeAlong/FileReader.java
+++ src/XtraOekter/Uke7/codeAlong/FileReader.java
@@ -1,11 +1,16 @@
 package XtraOekter.Uke7.codeAlong;
 
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.Scanner;
 
 public class FileReader {
     public void dummyMethod(){
         File myFile = new File("eksempel.txt");
-        Scanner myScanner = new Scanner(myFile);
+        try {
+            Scanner myScanner = new Scanner(myFile);
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
---
+++ src/XtraOekter/Uke7/codeAlong/MainReader.java
@@ -0,0 +1,8 @@
+package XtraOekter.Uke7.codeAlong;
+
+public class MainReader {
+    public static void main(String[] args) {
+        FileReader myReader = new FileReader();
+        myReader.dummyMethod();
+    }
+}
