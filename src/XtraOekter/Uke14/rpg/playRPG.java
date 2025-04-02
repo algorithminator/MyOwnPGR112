802e240603a0f685b5f98a110cddfd8d8be22b75
frdo004 <frederic.b.dorn@gmail.com>
2025-04-02 14:37:22 +0200

    transaksjon 2

--- src/XtraOekter/Uke14/rpg/playRPG.java
+++ src/XtraOekter/Uke14/rpg/playRPG.java
@@ -5,6 +5,9 @@ package XtraOekter.Uke14.rpg;
 
 
 
+import com.mysql.cj.jdbc.MysqlDataSource;
+import java.sql.*;
+
 import java.sql.SQLException;
 import java.util.Collection;
 import java.util.HashSet;
@@ -47,30 +50,49 @@ public class playRPG {
         System.out.println("Party 2 members:");
         bf.printPartStrength_Alt2(party2);
 
-        boolean fightLoop = true;
-        while (fightLoop) {
-            if (party1.size() == 0) fightLoop = false;
-            if (party2.size() == 0) fightLoop = false;
-            System.out.println("Party 1 members:");
-            bf.printPartStrength_Alt1(party1);
-            System.out.println("Party 2 members:");
-            bf.printPartStrength_Alt2(party2);
-            System.out.println("PARTY 1 wins this fight: " + bf.battle(party1, party2));
+        // bruk transakjoner - commit , rollback
+        MysqlDataSource rpgDS = rp.getRpgDS();
+        Connection con = rpgDS.getConnection();
+        boolean autoCommit = con.getAutoCommit();
+        con.setAutoCommit(false);
 
-            for (Character c : allPlayers) rp.updateRPGCharacters(c);
+        try {
+            boolean fightLoop = true;
+            while (fightLoop) {
+                if (party1.size() == 0) fightLoop = false;
+                if (party2.size() == 0) fightLoop = false;
+                System.out.println("Party 1 members:");
+                bf.printPartStrength_Alt1(party1);
+                System.out.println("Party 2 members:");
+                bf.printPartStrength_Alt2(party2);
+                System.out.println("PARTY 1 wins this fight: " + bf.battle(party1, party2));
 
-            for (Character c : allPlayers) {
-                if (c.getHealth() <= 0) {
-                    System.out.println("RIP " + c.getName());
-                    rp.deleteRPGCharacter(c);
-                    party1.remove(c);
-                    party2.remove(c);
-                    fightLoop = false;
+                for (Character c : allPlayers) rp.updateRPGCharacters(c, con);
+
+                for (Character c : allPlayers) {
+                    if (c.getHealth() <= 0) {
+                        System.out.println("RIP " + c.getName());
+                        rp.deleteRPGCharacter(c, con);
+
+                        party1.remove(c);
+                        party2.remove(c);
+                        fightLoop = false;
+
+                    }
 
                 }
-
             }
+            con.commit();
         }
+        catch (SQLException sqle){
+            System.out.println("Exception caught. Rolling back transaction.");
+            con.rollback();
+            throw sqle;
+        } finally {
+            // Setting the commit mode to what it was originally.
+            con.setAutoCommit(autoCommit);
+        }
+
 
 
         System.out.println("the end");
